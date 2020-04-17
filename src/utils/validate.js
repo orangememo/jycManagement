9523548c6b4a8b566a5b019e5fd6af1ced87caf2
Zhangq <1002591747@qq.com>
2020-04-17 11:38:00 +0800

    修改电话号码验证

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -32,7 +32,7 @@ export function validEmail(rule, value, callback) {
 export function validateMobile(rule, value, callback) {
 	if (typeof value === 'number') value = value.toString()
 	value = value.replace(/\s*/g, '')
-	let reg = /^(((13[0-9]{1})|(15[0-9]{1})|(16[0-9]{1})|(17[3-8]{1})|(18[0-9]{1})|(19[0-9]{1})|(14[5-7]{1}))+\d{8})$/
+	let reg = /^(((13[0-9]{1})|(14[0-9]{1})|(15[0-9]{1})|(16[0-9]{1})|(17[0-9]{1})|(18[0-9]{1})|(19[0-9]{1}))+\d{8})$/
 	if (reg.test(value)) {
 		callback()
 	} else {
